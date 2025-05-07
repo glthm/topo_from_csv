8720c9535f1a6e66793e799ff1973b4f84ff79e9
Guillaume Thomas <guillaume.thomas@inria.fr>
2025-05-07 09:47:03 +0200

    small work on stub

--- topo_from_csv/main.py
+++ topo_from_csv/main.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
 import polars as pl
+import sys
+from pathlib import Path
 
-
-def read_csv(input_path: str) -> pl.DataFrame: ...
-
-def remove_cols(df: pl.DataFrame, name: str, *more_names: str) -> pl.DataFrame: ...
+def grab_csv(path: Path|str) -> pl.DataFrame:
+    """
+    An interface for polars.read_csv that has the appropriate parameters for
+     our use case, and selects only the columns we need.
+    """
+    return pl.read_csv(
+                path,
+                columns=[
+                    "hold_colors",
+                    "grade",
+                    "name",
+                    "description",
+                    "openers",
+                    "anchor"
+                ],
+                separator="\t",
+                raise_if_empty=True
+            )
 
 def color_cols(
         df: pl.DataFrame,
@@ -13,12 +29,20 @@ def color_cols(
         *color_target_cols: str
 ) -> pl.DataFrame: ...
 
-def write_excel(output_path: str) -> None:
+def write_excel(output_path: str, df: pl.DataFrame) -> None:
     """
     https://docs.pola.rs/api/python/stable/reference/api/polars.DataFrame.write_excel.html
     """
     ...
 
-        
+def main():
+    path = Path(sys.argv[1])
+    df = pl.read_csv(
+            path,
+            separator="\t"
+        )
+    
+    write_excel(path.with_suffix(".xls"), df)
+
 if __name__ == "__main__":
   print("Only stubs are defined for now")
